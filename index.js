c8410e5e6e6c1a90ec00b389f3bdf6fc1816ec96
Jarvis9960 <ankitfukte11@gmail.com>
2023-12-20 15:45:37 +0530

    fixing backend issues

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import connectDb from "./Database/ConnectDb.js";
 import cors from "cors";
 import PlanningRoute from "../vitbackend/Routes/PlanningRoutes.js";
 import DesignRoute from "../vitbackend/Routes/DesignRoute.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", PlanningRoute);
 app.use("/api", DesignRoute);
